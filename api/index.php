caea0cb2e09aa05f888b76f2ff9b7e760b456c5a
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-11-07 17:00:29 +0630

    update

--- api/index.php
+++ api/index.php
@@ -66,7 +66,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guessonit</title>
-    <link rel="stylesheet" href="style.css?v=<?= time() ?>">
+    <link rel="stylesheet" href="../style.css?= time() ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Stack+Sans+Headline:wght@200..700&display=swap" rel="stylesheet">
